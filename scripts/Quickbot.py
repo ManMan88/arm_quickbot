2f7ccbbca9f69c97147f639c9f8747909634de9a
Ron Danon <rondanon@gmail.com>
2016-12-17 09:09:03 +0000

    Update Quickbot.py

--- scripts/Quickbot.py
+++ scripts/Quickbot.py
@@ -18,13 +18,13 @@ class IRSensors(object):
                                   1.267,1.397,1.562,1.763,2.017,2.335,2.72,2.994])
         self.distances = np.array([35,30,25,20,18,16,14,12,10,9,8,7,6,5,4,3.5]) 
         ADC.setup()
-            
+
     def _readRawValue(self):
         return ADC.read_raw(self.pin)
-        
+
     def irDist(self):
         return np.interp(self._readRawValue(),self.voltages,self.distances)
-    
+
     def terminate(self):
         ADC.cleanup()
 
@@ -40,20 +40,20 @@ class Encoder(object):
         self.velocity = 0
         self.dt = 1
         GPIO.setup(pin, GPIO.IN)
-    
+
     def _callbackCount(self):
         self.timeNew = time.clock()
         self.totalCount = self.totalCount + self.direction
-    
-    def reverseDirection(self, direction):       
+
+    def reverseDirection(self, direction):
         self.direction = -self.direction
-        
+
     def setInterrupt(self):
         GPIO.add_event_detect(self.pin, GPIO.RISING, callback=self._callbackCount, bouncetime=100)
-        
+
     def _shutdownInterrupt(self):
         GPIO.remove_event_detect(self.pin)
-    
+
     def calcCountAndVelocity(self):
         temp = [self.totalCount,self.timeNew]
         self.count = (temp[0] - self.lastCount)     #[counts]
@@ -62,27 +62,30 @@ class Encoder(object):
             self.velocity = self.count/self.dt          #[counts/msec]
         else:
             self.velocity = 0
-        self.lastTime = temp[1]        
+        self.lastTime = temp[1]
         self.lastCount = temp[0]
-    
+
     def terminate(self):
         self._shutdownInterrupt()
         GPIO.cleanup()
 
 class Motor(object):
-    def __init__(self,pinEn,pin1,pin2,maxVel=150):
+    def __init__(self,pinEn,pin1,pin2,maxVel=150,minVel=20):
         self.pineEn = pinEn
         self.pin1 = pin1
         self.pin2 = pin2
         self.targetVelocity = 0
         self.direction = 1
         self.maxVel = maxVel #[mm/sec] -> for both directions
+	self.minVal = minVal #[mm/sec] -> for both directions
+        self.diameter = diameter
+        self.baseLength = baseLength
         PWM.start(pinEn,0)
         GPIO.setup(pin1,GPIO.OUT)
         GPIO.setup(pin2,GPIO.OUT)
         GPIO.output(pin1,GPIO.HIGH)
         GPIO.output(pin2,GPIO.LOW)
-        
+
     def _reverseDirection(self):
         self.direction = - self.direction
         if self.direction > 0:
@@ -91,16 +94,16 @@ class Motor(object):
         else:
             GPIO.output(self.pin1,GPIO.LOW)
             GPIO.output(self.pin2,GPIO.HIGH)
-    
+
     def setTargetVelocity(self,desiredVelocity):
         #the desiredVelocity should be between [-maxVel,maxVel]
-        desiredDirection = p.sign(desiredVelocity)     
+        desiredDirection = p.sign(desiredVelocity)
         if abs(desiredVelocity) > maxVel:
             desiredVelocity = maxVel*desiredDirection
         if desiredDirection == - self.direction:
             self._reverseDirection()
         self.targetVelocity = desiredVelocity
-    
+
     def inputPWM(self, value):
         #the value should be between 0 and 100
         if value < 0:
@@ -108,7 +111,7 @@ class Motor(object):
         if value > 100:
             value = 100
         PWM.set_duty_cycle(self.pinEn,value)
-        
+
     def terminate(self):
         PWM.stop(self.pinEn)
         PWM.cleanup()
@@ -122,20 +125,22 @@ class Odometry(object):
         self.y = y0
         self.theta = theta0
 
-    def calcLocation(self,rightWheelDist,leftWheelDist):
-        distance = (rightWheelDist + leftWheelDist)*self.resolution/2
+    def calcLocation(self,rightWheelCount,leftWheelCount):
+        distance = (rightWheelCount + leftWheelCount)*self.resolution/2
         self.x = self.x + distance*cos(self.theta)
         self.y = self.y + distance*sin(self.theta)
-        self.theta = (rightWheelDist - leftWheelDist)*self.resolution/2
-        #also possible to solve with ODE on the velocity but I find it wastefull
+        self.theta = (rightWheelCount - leftWheelCount)*self.resolution/self.baseLength
 
 class Planner(object):
-    def __init__(self):
+    def __init__(self,weights=[1,4,8,4,1]):
         self.IRdata = [0,0,0,0,0]
         self.location = [0,0,0]
-        self.target = "Stop"
-        self.velCmd = 0
-        self.thetaCmd = 0
+	self.weights = weights
+        self.state = "Stop"
+	self.target = [0,0]
+	self.thetaDes = 0
+        self.velDes = 0
+	self.wallSide = 'right'
 
     def states(self):
         if state == "Stop":
@@ -152,12 +157,23 @@ class Planner(object):
 
     def _followWall(self):
         return 0
+
     def _goToTarget(self):
-        return 0
+	uX = self.target[0]-self.location[0]
+        uY = self.target[1]-self.location[1]
+        self.thetaDes = np.arctan2(uY,uX)
+	self.velDes = self._setVelocity()
+
     def _avoidObstacles(self):
         return 0
+
     def _stop(self):
-        return 0
+	self.thetaDes = 0
+        self.vDes = 0
+
+    def _setVelocity():
+	#need to add some function of IR and target distance
+	return 0.5
 
 class StateMachine(object):
     def __init(self, stopError=20, emergancyDist=4, avoidObstaclesDist=10, followWallDist=22, wallBias=30, factorIR=[1,5,10,5,1], target=[0,0]):
@@ -175,7 +191,7 @@ class StateMachine(object):
         self.followWallDist = followWallDist
         self.emergancyDist = emergancyDist
         self.wallBias = wallBias
-        
+
     def setTarget(self,target):
         self.target = target
         self.distanceFromTargert = np.linalg.norm(self.location[0:1] - self.target)
@@ -190,7 +206,7 @@ class StateMachine(object):
         if self.distanceFromTargert < self.stopError:
             return 1
         return 0
-    
+
     def _checkAvoidObstacles(self):
         if self.minIRdata < self.avoidObstaclesDist:
             return 1
@@ -205,12 +221,9 @@ class StateMachine(object):
     def _checkStopFollowWall(self):
         if self.distanceFromTargert + self.wallBias < self.distanceStartWall:
             return 1
-        
         return 0
 
 
-
-    
     def chooseState(self):
         self.distanceFromTargert = np.linalg.norm(self.location[0:1] - self.target)
         self.minIRdata = min(IRdata)
@@ -229,5 +242,5 @@ class StateMachine(object):
             self.followWall = 1
         else:
             self.state = "GoToTarget"
-            
+
         return self.state
